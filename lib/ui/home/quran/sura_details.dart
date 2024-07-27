a33ea1130fcc069c0833ca73a64b3b1f8e23cb76
abdulrahman <zezobode430@gmail.com>
2024-07-27 13:11:43 +0300

    feat: sura details logic

--- lib/ui/home/quran/sura_details.dart
+++ lib/ui/home/quran/sura_details.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:islami_app/core/theme/my_theme.dart';
 import 'package:islami_app/core/widgets/default_screen.dart';
 import 'package:islami_app/ui/home/quran/sura_details_args.dart';
+import 'package:islami_app/ui/home/quran/verse_content.dart';
 
-class SuraDetails extends StatelessWidget {
+class SuraDetails extends StatefulWidget {
   static const String routeName = "sura_details";
+
+  const SuraDetails({super.key});
+
+  @override
+  State<SuraDetails> createState() => _SuraDetailsState();
+}
+
+class _SuraDetailsState extends State<SuraDetails> {
+  List<String> verses = [];
 
   @override
   Widget build(BuildContext context) {
     var args = ModalRoute.of(context)?.settings.arguments as SuraDetailsArgs;
+
+    if (verses.isEmpty) {
+      readFileData(args.suraIndex);
+    }
+
     return DefaultScreen(
       body: Scaffold(
         appBar: AppBar(
@@ -23,14 +40,35 @@ class SuraDetails extends StatelessWidget {
           ),
           elevation: 3.5,
           margin: const EdgeInsets.symmetric(vertical: 55, horizontal: 24),
-          child: ListView.builder(
-            itemCount: 100,
-            itemBuilder: (context, index) {
-              return Text("Details");
-            },
-          ),
+          child: verses.isNotEmpty
+              ? ListView.separated(
+                  separatorBuilder: (context, index) => const Divider(
+                    color: MyThemeData.lightPrimary,
+                    thickness: 2,
+                    indent: 40,
+                    endIndent: 40,
+                    height: 1,
+                  ),
+                  itemCount: verses.length,
+                  itemBuilder: (context, index) {
+                    return VerseContent(verses[index], index);
+                  },
+                )
+              : const Center(
+                  child: CircularProgressIndicator(
+                    color: MyThemeData.lightPrimary,
+                  ),
+                ),
         ),
       ),
     );
+  }
+
+  void readFileData(int fileIndex) async {
+    String fileContent =
+        await rootBundle.loadString("assets/files/${fileIndex + 1}.txt");
+    List<String> lines = fileContent.trim().split("\n");
+    verses = lines;
+    setState(() {});
   }
 }
